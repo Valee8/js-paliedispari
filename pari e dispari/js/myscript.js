6417be56eb3ac26dcad8362e067d0800a22f00d2
Vale <oirelav95@gmail.com>
2022-11-15 20:47:07 +0100

    aggiungo return a funzione pariODispari

--- pari e dispari/js/myscript.js
+++ pari e dispari/js/myscript.js
@@ -9,6 +9,8 @@ let minComputer = 1;
 
 let maxComputer = 5;
 
+let risultatoGioco;
+
 let sceltaUtente = prompt("Scegli pari o dispari (p o d)");
 
 while (sceltaUtente != "p" && sceltaUtente != "d") {
@@ -33,7 +35,14 @@ const sommaNumeri = numeroComputer + numeroUtente;
 
 console.log("Somma dei numeri: ", sommaNumeri);
 
-pariODispari(sommaNumeri, sceltaUtente);
+risultatoGioco = pariODispari(sommaNumeri, sceltaUtente, risultatoGioco);
+
+if (risultatoGioco) {
+    console.log("Hai vinto");
+}
+else {
+    console.log("Hai perso");
+}
 
 
 // Funzione per generare numero random
@@ -44,14 +53,16 @@ function generaNumeroRandom(min, max) {
 }
 
 // Funzione per controllare se la somma è pari o dispari
-function pariODispari(somma, scelta) {
+function pariODispari(somma, scelta, risultato) {
 
     console.log("Scelta ", scelta);
 
     if ((somma % 2 === 0 && scelta === "p") || (somma % 2 != 0 && scelta === "d")) {
-        console.log("Hai vinto");
+       risultato = true;
     }
     else {
-        console.log("Hai perso");
+        risultato = false;
     }
+
+    return risultato;
 }
